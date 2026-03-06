3e4b074c06b77275ec9850f274c8dfbf5b583c7f
Maciej Januś <yanuu.why@gmail.com>
2026-03-06 12:47:13 +0100

    feat: automatically create personal calendar and revalidate path on sign-in

--- src/server/better-auth/config.ts
+++ src/server/better-auth/config.ts
@@ -4,8 +4,13 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { env } from "../../env";
 import { db } from "../../server/db";
 import { sendVerificationEmail } from "../../server/mail/mailing";
+import { createAuthMiddleware } from "better-auth/api";
+import { ClassGroupRole, ClassGroupType } from "generated/prisma";
+import { revalidatePath } from "next/cache";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
+  plugins: [nextCookies()],
   database: prismaAdapter(db, {
     provider: "postgresql", // or "sqlite" or "mysql"
   }),
@@ -28,6 +33,35 @@ export const auth = betterAuth({
     },
     sendOnSignIn: true,
     expiresIn: 1000 * 60 * 60 * 24 * 3, // 3 days
+  },
+  hooks: {
+    after: createAuthMiddleware(async (ctx) => {
+      if (ctx.path.startsWith("/sign-up")) {
+        const newSession = ctx.context.newSession;
+        if (newSession) {
+          await db.classGroup.create({
+            data: {
+              id: crypto.randomUUID(),
+              name: `${newSession.user.name}'s Personal Calendar`,
+              description: `This is your personal calendar. Nobody can join - create a group to share.`,
+              password: undefined,
+              createdById: newSession.user.id,
+              type: ClassGroupType.PERSONAL,
+              members: {
+                create: {
+                  userId: newSession.user.id,
+                  role: ClassGroupRole.OWNER,
+                  isVisible: true,
+                },
+              },
+            },
+          });
+        }
+      }
+      if (ctx.path.startsWith("/sign-in")) {
+        revalidatePath("/", "page");
+      }
+    }),
   },
 });
 
